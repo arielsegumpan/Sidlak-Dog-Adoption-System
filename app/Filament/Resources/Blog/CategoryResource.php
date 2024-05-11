09c4fc8d619b24e15d203abe3bb66446b5afa76b
arielsegumpan <arielsegumpan31@gmail.com>
2024-05-11 16:09:17 +0800

    add category model, resource and factory

---
+++ app/Filament/Resources/Blog/CategoryResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources\Blog;
+
+use App\Filament\Resources\Blog\CategoryResource\Pages;
+use App\Filament\Resources\Blog\CategoryResource\RelationManagers;
+use App\Models\Blog\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-squares-plus';
+
+    protected static ?string $navigationGroup = 'Posts';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->deferLoading()
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make()
+                ->icon('heroicon-m-plus')
+                ->label(__('Create Category')),
+            ])
+            ->emptyStateIcon('heroicon-o-squares-plus')
+            ->emptyStateHeading('No categories are created');;
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
